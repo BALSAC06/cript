22f651e0b6e963efbaa250286b43b1587234a45e
Car1014 <carlosroo1014@gmail.com>
2023-09-29 15:55:51 -0500

    segundo

--- src/views/Location.vue
+++ src/views/Location.vue
@@ -1,5 +1,25 @@
 <template>
-<div>Location</div>
+    <div>Location</div>
+
+    <div style="" class="contenedor">
+        <section  style="padding-bottom: 1rem; padding-top:1rem ; display: block; text-align: center; margin: 1rem; color :green; border: solid green 2px;" v-for="value in character" >
+
+            <div class="contendor_hijo">
+            <img v-bind:src="value.image" alt="" width="150" height="150" >
+        
+            <div style="padding-top: 1.5rem;">    
+                {{ value.name }}
+            </div>
+
+            <div style="padding-top: 1rem;">
+                {{value.type }}
+            
+            </div>
+        </div>
+
+
+        </section>
+    </div>
 </template>
 
 <script setup>
@@ -7,7 +27,7 @@ import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
 onMounted(() => {
-      
+    getCharacter(); 
 })
 
 
@@ -16,8 +36,7 @@ const getCharacter= async()=>{
     await axios.get('https://rickandmortyapi.com/api/character')
 
     .then((resolve)=> {
-
-        console.log(resolve);
+        character.value=resolve.data.results
     }).catch((error)=>{
         console.log(error);
     })
@@ -27,4 +46,14 @@ const getCharacter= async()=>{
 </script>
 
 <style>
+
+.contenedor{
+    display: grid;
+    grid-template-columns: 320px 320px 320px;
+    align-items: center;
+}
+.contenedor_hijo{
+    padding: 10px;
+}
+
 </style>
